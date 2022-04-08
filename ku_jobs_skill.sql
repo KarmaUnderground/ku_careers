dc1cc615b95d4cf88d64c7ed5b4ccbeb00cdb219
manutrix <patrickbeaudoin@manutrix.com>
2022-04-08 16:47:45 -0400

    Change limit to weight according to ex_extended module

--- ku_jobs_skill.sql
+++ ku_jobs_skill.sql
@@ -1,16 +1,8 @@
 USE `essentialmode`;
 
-CREATE TABLE `user_skills` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `identifier` varchar(255) COLLATE utf8mb4_bin NOT NULL,
-  `name` varchar(255) COLLATE utf8mb4_bin NOT NULL,
-  `level` int(11) NOT NULL DEFAULT 0,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
-
-INSERT INTO `items` (`name`, `label`, `limit`, `rare`, `can_remove`) VALUES ('gas', 'Essence', '200', '0', '1');
-INSERT INTO `items` (`name`, `label`, `limit`, `rare`, `can_remove`) VALUES ('refined_oil', 'Pétrole Raffiné', '200', '0', '1');
-INSERT INTO `items` (`name`, `label`, `limit`, `rare`, `can_remove`) VALUES ('oil', 'Pétrole', '200', '0', '1');
+INSERT INTO `items` (`name`, `label`, `weight`, `rare`, `can_remove`) VALUES ('gas', 'Essence', '200', '0', '1');
+INSERT INTO `items` (`name`, `label`, `weight`, `rare`, `can_remove`) VALUES ('refined_oil', 'Pétrole Raffiné', '200', '0', '1');
+INSERT INTO `items` (`name`, `label`, `weight`, `rare`, `can_remove`) VALUES ('oil', 'Pétrole', '200', '0', '1');
 
 INSERT INTO `jobs` (`name`, `label`) VALUES ('gas', 'Essence');
 
